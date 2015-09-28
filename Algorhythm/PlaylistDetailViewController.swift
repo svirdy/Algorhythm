f316f1d4620a1801532109a7334d4705e1926cce
Sachin Virdy <svirdy1@gmail.com>
2015-09-27 20:44:29 -0700

    move segue from button placeholder to image

--- Algorhythm/PlaylistDetailViewController.swift
+++ Algorhythm/PlaylistDetailViewController.swift
@@ -11,14 +11,31 @@ import UIKit
 class PlaylistDetailViewController: UIViewController {
 
     
-    @IBOutlet weak var buttonPressLabel: UILabel!
     var playlist: Playlist?
+    
+    @IBOutlet weak var playlistCoverImage: UIImageView!
+    @IBOutlet weak var playlistTitle: UILabel!
+    @IBOutlet weak var playlistDescription: UILabel!
+    
+    @IBOutlet weak var playlistArtist0: UILabel!
+    @IBOutlet weak var playlistArtist1: UILabel!
+    @IBOutlet weak var playlistArtist2: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if playlist != nil {
-            buttonPressLabel.text = playlist!.title
+            
+            playlistCoverImage.image = playlist!.largeIcon
+            playlistCoverImage.backgroundColor = playlist!.backgroundColor
+            playlistTitle.text = playlist!.title
+            playlistDescription.text = playlist!.description
+            
+            playlistArtist0.text = playlist!.artists[0]
+            playlistArtist1.text = playlist!.artists[1]
+            playlistArtist2.text = playlist!.artists[2]
+            
         }
     }
 
--- Algorhythm/PlaylistMasterViewController.swift
+++ Algorhythm/PlaylistMasterViewController.swift
@@ -10,13 +10,11 @@ import UIKit
 
 class PlaylistMasterViewController: UIViewController {
 
-    @IBOutlet weak var aButton: UIButton!
     @IBOutlet weak var playlistImageView0: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        aButton.setTitle("Press me!", forState: .Normal)
         let playlist = Playlist(index: 0)
         playlistImageView0.image = playlist.icon
     
@@ -28,11 +26,15 @@ class PlaylistMasterViewController: UIViewController {
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showPlaylistDetail" {
+        if segue.identifier == "showPlaylistDetailSegue" {
             let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
             playlistDetailController.playlist = Playlist(index: 0)
         }
     }
 
+    @IBAction func showPlaylistDetail(sender: AnyObject) {
+        
+        performSegueWithIdentifier("showPlaylistDetailSegue", sender: sender)
+    }
 }
 
